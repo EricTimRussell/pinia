cadb2181bacf323db06bb64b064d496ae36b7548
Eric Russell <Erictim.Russell@live.com>
2023-01-24 11:35:05 -0700

    init push pinia

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <main>
+    <header>
+      <img src="https://thiscatdoesnotexist.com/" alt="Cat">
+      <h1>Pinia Tasks</h1>
+    </header>
+
+    <!-- task list -->
+    <div class="task-list">
+      <div v-for="t in taskStore.tasks">
+        <TaskDetails :t="t" />
+      </div>
+    </div>
+
+  </main>
+</template>
+
+<script>
+import TaskDetails from './components/TaskDetails.vue'
+import { useTaskStore } from './stores/TaskStore'
+export default {
+  components: { TaskDetails },
+  setup() {
+    const taskStore = useTaskStore()
+    return { taskStore }
+  }
+}
+</script>
+
+
